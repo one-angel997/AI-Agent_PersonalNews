a00919154735731c8f51900a71682fdda532c9c1
one-angel997 <one_angelo@hotmail.com>
2026-03-31 18:18:08 +0200

    Delete .shortcuts directory

--- .shortcuts/start_news.sh
+++
@@ -1,3 +0,0 @@
-#!/data/data/com.termux/files/usr/bin/sh
-python ~/news_agent/news_agent.py > ~/news_agent/output.txt
-termux-notification --id technews --title "Tech News" --content "$(cat ~/news_agent/output.txt)"
